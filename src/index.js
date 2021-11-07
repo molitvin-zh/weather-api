6e770f66b27e61520854963cd994794a6a22bb74
molitvin <molitvin.web@gmail.com>
2021-11-08 00:39:58 +0400

    corrected mistakes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+//import Forecast from '@models/Forecast.js';
+
 import 'normalize.css';
 import "@/styles/styles.scss";
 
-import axios from 'axios'
-//import DayCardsInfo from "@/models/DayCardsInfo.js";
+import axios from 'axios';
 
-window.initMap = () => {
+window.searchForecast = () => {
   const input = document.querySelector('.search__input');
   const autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.addListener("place_changed", () => {
@@ -16,8 +17,8 @@ window.initMap = () => {
     }
 
     const params = {
-      lat: `${place.geometry.location.lat().toFixed(1)}`,
-      lon: `${place.geometry.location.lng().toFixed(1)}`,
+      lat: `${place.geometry.location.lat()}`,
+      lon: `${place.geometry.location.lng()}`,
       key: '5086cdd8745d497fa07f6786dafbbcdc'
     }
     createForecast(params);
@@ -26,11 +27,11 @@ window.initMap = () => {
 };
 
 function createForecast(params) {
-  axios.get('https://api.weatherbit.io/v2.0/forecast/daily', {
-    params: params
-  }).then(response => {
-    console.log(response.data)
-  }).catch(error => {
-    console.error(error);
-  });
+  axios.get('https://api.weatherbit.io/v2.0/forecast/daily', { params })
+    .then(response => {
+      //const forecast = new Forecast(response.data.data);
+      //forecast.createForecastBlocks();
+    }).catch(error => {
+      console.error(error);
+    });
 }
